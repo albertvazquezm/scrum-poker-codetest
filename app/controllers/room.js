335e94a67b27a944a61ee9224792a82eef941559
Albert Vazquez <hola@albertvazquez.es>
2015-05-25 13:15:31 +0200

    Room controller

--- app/controllers/room.js
+++ app/controllers/room.js
@@ -3,8 +3,93 @@ var express = require('express'),
   mongoose = require('mongoose'),
   Room = mongoose.model('Room');
 
-  module.exports = function(app){
-    // exports.open = function(a){
-    //   console.log(a);
-    // }
+exports.get = function(req, res, next) {
+  Room.findById(req.params.code, function(err, room) {
+    if (err) {
+      res.json('404', {
+        'error': 'Not found'
+      });
+    }
+    res.json({
+      'room': room
+    });
+  });
+}
+
+exports.create = function(req, res, next) {
+  var room = new Room();
+  room.save(function(err) {
+    if (err) {
+      res.json('500', {
+        'error': 'Error creating room'
+      });
+      return false;
+    } else {
+      res.json({
+        'room': room
+      });
+    }
+  })
+}
+
+exports.connectUser = function(roomId, cb) {
+  Room.findById(roomId, function(err, room) {
+    if (err) {
+      cb(true);
+    }
+    if (!room.people) {
+      room.people = 1;
+    } else {
+      room.people++;
+    }
+    if (room.save()) {
+      cb(false, room.people);
+    } else {
+      cb(true);
+    }
+  });
+}
+
+exports.disconnectUser = function(roomId, cb) {
+  if (roomId) {
+    Room.findById(roomId, function(err, room) {
+      if (err) {
+        cb(true);
+      }
+      if (!room.people) {
+        room.people = 0;
+      } else {
+        room.people = room.people - 1;
+      }
+
+      room.save(function(err) {
+        if (err) {
+          cb(true);
+        } else {
+          cb(false, room.people);
+        }
+      });
+    });
+  } else {
+    cb(true);
   }
+}
+
+exports.addVote = function(roomId, number, cb) {
+  Room.findById(roomId, function(err, room) {
+    if (err) {
+      cb(true);
+    }
+    room.votes.push(number);
+    if (room.save()) {
+      if(room.votes.length === room.people){
+        cb(false, room.votes, true);
+      }else{
+        cb(false, room.votes);
+      }
+
+    } else {
+      cb(false);
+    }
+  });
+}
