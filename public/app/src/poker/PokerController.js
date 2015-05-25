8a8a95d3ae34c9797ff1655f478d3fa30ea00e27
Albert Vazquez <hola@albertvazquez.es>
2015-05-25 13:17:11 +0200

    Add votes and end support to Public Poker Controller

--- public/app/src/poker/PokerController.js
+++ public/app/src/poker/PokerController.js
@@ -17,15 +17,25 @@
     vm.goToCode = goToCode;
     vm.newRoom = newRoom;
     vm.choose = choose;
+    vm.votes = [];
+    vm.connectedClients = 0;
     vm.err = null;
+    vm.end = false;
 
-    socket.on('clients', function(data){
-      console.log(data);
-    })
+    socket.on('clients', function(clients){
+      vm.connectedClients = clients;
+    });
+
+    socket.on('votes', function(votes){
+      vm.votes = votes;
+    });
+
+    socket.on('end', end)
 
     function goToCode() {
       resetErr();
       resetRoom();
+      closeRoom();
       $http.get('/api/room/' + vm.code).then(function(data) {
         openRoom(data.data.room);
       }, function(err) {
@@ -48,8 +58,17 @@
       socket.emit('open',room._id);
     }
 
+    function closeRoom(){
+      if(vm.room._id){
+        socket.emit('close',vm.room._id);
+      }
+    }
+
     function choose(idx){
-      vm.choosed = idx;
+      if(!vm.end && !vm.choosed){
+        vm.choosed = idx;
+        socket.emit('vote',vm.cards[idx]);
+      }
     }
 
     function resetErr(){
@@ -58,6 +77,10 @@
 
     function resetRoom(){
       vm.room = {};
+      vm.end = false;
+      vm.connectedClients = 0;
+      vm.votes = [];
+      vm.choosed = null;
     }
 
     function generateCards(){
@@ -76,82 +99,9 @@
       ]
     }
 
-    // var self = this;
-    //
-    // self.selected     = null;
-    // self.users        = [ ];
-    // self.selectUser   = selectUser;
-    // self.toggleList   = toggleUsersList;
-    // self.showContactOptions  = showContactOptions;
-    //
-    // // Load all registered users
-    //
-    // userService
-    //       .loadAllUsers()
-    //       .then( function( users ) {
-    //         self.users    = [].concat(users);
-    //         self.selected = users[0];
-    //       });
-    //
-    // // *********************************
-    // // Internal methods
-    // // *********************************
-    //
-    // /**
-    //  * First hide the bottomsheet IF visible, then
-    //  * hide or Show the 'left' sideNav area
-    //  */
-    // function toggleUsersList() {
-    //   var pending = $mdBottomSheet.hide() || $q.when(true);
-    //
-    //   pending.then(function(){
-    //     $mdSidenav('left').toggle();
-    //   });
-    // }
-    //
-    // /**
-    //  * Select the current avatars
-    //  * @param menuId
-    //  */
-    // function selectUser ( user ) {
-    //   self.selected = angular.isNumber(user) ? $scope.users[user] : user;
-    //   self.toggleList();
-    // }
-    //
-    // /**
-    //  * Show the bottom sheet
-    //  */
-    // function showContactOptions($event) {
-    //     var user = self.selected;
-    //
-    //     return $mdBottomSheet.show({
-    //       parent: angular.element(document.getElementById('content')),
-    //       templateUrl: '/src/users/view/contactSheet.html',
-    //       controller: [ '$mdBottomSheet', ContactPanelController],
-    //       controllerAs: "cp",
-    //       bindToController : true,
-    //       targetEvent: $event
-    //     }).then(function(clickedItem) {
-    //       clickedItem && $log.debug( clickedItem.name + ' clicked!');
-    //     });
-    //
-    //     /**
-    //      * Bottom Sheet controller for the Avatar Actions
-    //      */
-    //     function ContactPanelController( $mdBottomSheet ) {
-    //       this.user = user;
-    //       this.actions = [
-    //         { name: 'Phone'       , icon: 'phone'       , icon_url: 'assets/svg/phone.svg'},
-    //         { name: 'Twitter'     , icon: 'twitter'     , icon_url: 'assets/svg/twitter.svg'},
-    //         { name: 'Google+'     , icon: 'google_plus' , icon_url: 'assets/svg/google_plus.svg'},
-    //         { name: 'Hangout'     , icon: 'hangouts'    , icon_url: 'assets/svg/hangouts.svg'}
-    //       ];
-    //       this.submitContact = function(action) {
-    //         $mdBottomSheet.hide(action);
-    //       };
-    //     }
-    // }
-
+    function end(){
+      vm.end = true;
+    }
   }
 
 })();
